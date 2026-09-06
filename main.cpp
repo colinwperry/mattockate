3792e924c3ab3e1e86b50630c5c7a974f34d75bf
Colin Perry <colin.w.perry@proton.me>
2026-09-05 22:04:37 -0400

    Refactor StackAllocator class for improved readability and structure

--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 template <size_t Capacity>
 class StackAllocator
 {
-    private: 
-        std::byte buffer_[Capacity];
-        size_t offset_ {0};
+private:
+    alignas(std::max_align_t) std::byte buffer_[Capacity];
+    size_t offset_{0};
+
+public:
+    void reset()
+    {
+        offset_ = 0;
+    }
+    void *alloc(size_t size, size_t alignment)
+    {
+
+        // calculates the aligned offset according to alignment padding
+        size_t alignedOffset{offset_ + (alignment - (offset_ % alignment)) % alignment};
+
+        // edge case for running out of memory
+        if (alignedOffset + size > Capacity)
+        {
+            return nullptr;
+        }
+        else
+        {
+            // reassigns offset_ accordingly
+            offset_ = alignedOffset;
+            void *startingPoint{&buffer_[offset_]};
+            offset_ += size;
+            return startingPoint;
+        }
+    }
 };
